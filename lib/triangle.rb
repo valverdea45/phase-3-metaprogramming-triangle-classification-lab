13ae33b8fec3e47e2ba5b0e7d938c5b8fac4a7ce
Adrian <adrianvalverde45@gmail.com>
2023-06-16 20:04:36 -0400

    commit tree 84fb80d3319df7617431144434bb781892ba7efe

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+
+    if @length_one <= 0 || @length_two <= 0 || @length_three <= 0
+      raise TriangleError
+      elsif @length_one + @length_two <= @length_three || @length_two + @length_three <= @length_one || @length_one + @length_three <= @length_two
+        raise TriangleError
+      elsif @length_one == @length_two && @length_two == @length_three
+        :equilateral
+      elsif [@length_one, @length_two, @length_three].uniq.length == 2
+        :isosceles
+      else [@length_one, @length_two, @length_three].uniq.length == 3
+        :scalene
+      end
+  end
+
+  
+
 end
